a44e240324932f402d5e9367be973896df96cf07
Tomas863 <62059264+Tomas863@users.noreply.github.com>
2024-10-17 14:09:04 -0300

    Actualización del modelo
    Se ajustó el modelo para coincidir con el nuevo modelado planteado

--- creacion_bdd.sql
+++ creacion_bdd.sql
@@ -7,7 +7,6 @@ go
 create schema usuario;
 go
 
-
 -- esquema de usuarios
 create table usuario.empleado (
     id_empleado int primary key identity(1,1),
@@ -15,7 +14,7 @@ create table usuario.empleado (
     apellido varchar(100),
     email varchar(200),
     contraseña varchar(100)
-    -- foto VARBINARY(MAX),
+    -- foto_perfil VARBINARY(MAX),
     -- ruta_foto varchar(300)
 );
 
@@ -24,8 +23,8 @@ create table usuario.supervisor (
     nombre varchar(100),
     apellido varchar(100),
     email varchar(200),
-    contraseña varchar(100)
-    -- foto VARBINARY(MAX),
+    contraseña varchar(100),
+    -- foto_perfil VARBINARY(MAX),
     -- ruta_foto varchar(300)
 );
 
@@ -35,24 +34,14 @@ create table usuario.rel_empleado_supervisor (
     id_empleado int
 );
 
-create table usuario.rel_proyecto_empleado (
-    id_rel int primary key identity(1,1),
-    id_empleado int,
-    id_proyecto int
-);
-
-create table usuario.rel_empleado_tarea (
-    id_asignacion_empleado_tarea int primary key identity(1,1),
-    id_tarea int,
-    id_empleado int
-);
-
 create table usuario.detalle_horas_trabajo (
     id_detalle int primary key identity(1,1),
-    id_asignacion_empleado_tarea int,
     hora_inicio_trabajo time,
     hora_fin_trabajo time,
-    descripcion_hora_trabajo varchar(1500)
+    descripcion_hora_trabajo varchar(1500),
+	id_empleado int,
+	id_actividad int,
+	id_proyecto int
 );
 
 -- esquema de labores
@@ -64,17 +53,9 @@ create table labor.proyecto (
     fecha_fin datetime
 );
 
-create table labor.tarea (
-    id_tarea int primary key identity(1,1),
-    descripcion varchar(1500),
-    fecha_inicio datetime,
-    fecha_fin datetime
-);
-
-create table labor.rel_proyecto_tarea (
-    id_rel int primary key identity(1,1),
-    id_proyecto int,
-    id_tarea int
+create table labor.actividad (
+    id_actividad int primary key identity(1,1),
+    actividad varchar(100),
 );
 
 -- foreign keys usuario
@@ -86,46 +67,16 @@ ALTER TABLE usuario.rel_empleado_supervisor
 ADD FOREIGN KEY (id_empleado)
 REFERENCES usuario.empleado (id_empleado);
 
-ALTER TABLE usuario.rel_proyecto_empleado
+ALTER TABLE usuario.detalle_horas_trabajo
 ADD FOREIGN KEY (id_empleado)
 REFERENCES usuario.empleado (id_empleado);
-
-ALTER TABLE usuario.rel_proyecto_empleado
-ADD FOREIGN KEY (id_proyecto)
-REFERENCES labor.proyecto (id_proyecto);
-
-
-ALTER TABLE usuario.rel_empleado_tarea
-ADD FOREIGN KEY (id_tarea)
-REFERENCES labor.tarea (id_tarea);
-
-ALTER TABLE usuario.rel_empleado_tarea
-ADD FOREIGN KEY (id_empleado)
-REFERENCES usuario.empleado (id_empleado);
-
-ALTER TABLE usuario.rel_empleado_tarea
-ADD FOREIGN KEY (id_empleado)
-REFERENCES usuario.empleado (id_empleado);
-
 
 ALTER TABLE usuario.detalle_horas_trabajo
-ADD FOREIGN KEY (id_asignacion_empleado_tarea)
-REFERENCES usuario.rel_empleado_tarea (id_asignacion_empleado_tarea);
-
-
--- foreign keys labor
-
-ALTER TABLE labor.rel_proyecto_tarea
 ADD FOREIGN KEY (id_proyecto)
 REFERENCES labor.proyecto (id_proyecto);
 
-
-ALTER TABLE labor.rel_proyecto_tarea
-ADD FOREIGN KEY (id_tarea)
-REFERENCES labor.tarea (id_tarea);
-
-
+ALTER TABLE usuario.detalle_horas_trabajo
+ADD FOREIGN KEY (id_actividad)
+REFERENCES labor.actividad (id_actividad);
 
 -- use tempdb;  drop database gtimesheet
-
-
